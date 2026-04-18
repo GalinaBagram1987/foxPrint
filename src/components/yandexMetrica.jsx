74e7981eab001a8106602d093eca94440c6d3192
GalinaBagram1987 <gaby_ev@mail.ru>
2026-04-18 18:19:48 +1000

    return yandexMetrica fuction

--- src/components/yandexMetrica.jsx
+++ src/components/yandexMetrica.jsx
@@ -1,21 +1,28 @@
 const loadYandexMetrika = () => {
-  if (window.ym) return;
+   if (window.ym) return; // уже есть
+
   const script = document.createElement('script');
-  script.src = "https://mc.yandex.ru/metrika/tag.js";
-  script.async = true;
-  script.onload = () => {
-    window.ym(107195914, 'init', {
-      clickmap: true,
-      trackLinks: true,
-      accurateTrackBounce: true,
-      webvisor: true,
-    });
-  };
+  script.textContent = `
+    (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j=0; j<document.scripts.length; j++) {
+        if (document.scripts[j].src === r) return;
+      }
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0];
+      k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  `;
   document.head.appendChild(script);
+
+  // Теперь можно смело вызывать ym, он будет в очереди
+  window.ym(107195914, 'init', {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true,
+  });
 };
-
-
-
 
 
 export default loadYandexMetrika;
